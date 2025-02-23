cca461c11805a4a607aa1a585ede82f1cd3bd8ae
pankajkumar24-dev <pkumargithub@gmail.com>
2025-02-23 23:48:20 +0530

    Add project files.

---
+++ Aspnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aspnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
